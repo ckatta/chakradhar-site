c9764307c61cd5395435239d4db0eab1f76097f2
Chakradhar Katta <ckatta@americanportfolios.com>
2021-09-28 04:33:37 +0000

    modified content

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <div style={{minWidth:1000}} className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
